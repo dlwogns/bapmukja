23763c814c751b46c42b483a025bb6da1381cedc
leejaehun <ldyjka@gmail.com>
2023-05-11 03:11:19 +0900

    initial commit

---
+++ src/main/java/com/example/bapmukja/BapmukjaApplication.java
@@ -0,0 +1,13 @@
+package com.example.bapmukja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapmukjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapmukjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bapmukja/BapmukjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bapmukja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BapmukjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
